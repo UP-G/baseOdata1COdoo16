3e1d516b94b41dd8a623edb5cd686114aaa982c6
root <root@odoo.tmtr.ru>
2023-05-16 13:25:02 +0300

    added controllers debts

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,8 @@
     'data': [
         'security/ir.model.access.csv',
         'views/user_1c_view.xml',
-        'views/config_1c_view.xml',
+        'views/odata_1c_connection_view.xml',
+        'views/odata_1c_route_view.xml',
         'views/odata_1c_menu_views.xml',
     ],
     'installable': True,
--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 from . import user_1c
-from . import config_1c
+from . import odata_1c_connetion
+from . import odata_1c_route
